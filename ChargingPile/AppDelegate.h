6e7599de0f3282b85656e52e6add7c80685d1317
RobinLiu <RobinLiu@liuxiaodandemacbook-pro.local>
2016-08-29 09:09:29 +0800

    Initial Commit

---
+++ ChargingPile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChargingPile
+//
+//  Created by RobinLiu on 16/8/29.
+//  Copyright © 2016年 chargingPile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
